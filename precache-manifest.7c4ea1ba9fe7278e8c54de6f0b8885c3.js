b44d1f48a657f85b74e55a31c30866bfa36cacb8
Marius Brataas <mariusbrataas@me.com>
2019-08-23 00:06:18 +0200

    Updates

---
+++ precache-manifest.7c4ea1ba9fe7278e8c54de6f0b8885c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842834a0fb814b4ed06e112ddd5201b9",
+    "url": "/gradient_tool/index.html"
+  },
+  {
+    "revision": "b96308b9cfe3dabcdfc0",
+    "url": "/gradient_tool/static/css/main.798bc5ec.chunk.css"
+  },
+  {
+    "revision": "cf16d940e33e7ef66339",
+    "url": "/gradient_tool/static/js/2.30689618.chunk.js"
+  },
+  {
+    "revision": "b96308b9cfe3dabcdfc0",
+    "url": "/gradient_tool/static/js/main.06b4fc3b.chunk.js"
+  },
+  {
+    "revision": "92578474a2806b765f88",
+    "url": "/gradient_tool/static/js/runtime~main.28b50643.js"
+  }
+]);
